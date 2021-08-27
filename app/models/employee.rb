5b75c40e00a9f66a4ea2c2c92edaf61b73f1b199
Sumit Israni <sumitisrani88@gmail.com>
2021-08-27 16:56:44 +0530

    Added age column in employees.

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,9 @@
 class Employee < ApplicationRecord
   validates :name, :email, :dob, :salary, presence: true
   validates :salary, numericality: true
+
+  def age
+    today = Date.current
+    today.year - dob.year - ((today.month > dob.month || (today.month == dob.month && today.day >= dob.day)) ? 0 : 1)
+  end
 end
